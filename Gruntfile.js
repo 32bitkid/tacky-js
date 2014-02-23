74c3083a3c76cb2eab374bdb28e140c831e7daf1
James Holmes <32bitkid@gmail.com>
2014-02-23 01:54:02 +0000

    Adding docco for documentation generation

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,6 @@
 module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-docco');
   
   // Project configuration.
   grunt.initConfig({
@@ -7,6 +8,14 @@ module.exports = function(grunt) {
       all: {
         files: {
           'tacky-min.js': ['tacky.js']
+        }
+      }
+    },
+    docco: {
+      debug: {
+        src: ['tacky.js'],
+        options: {
+          output: 'docs/'
         }
       }
     }
